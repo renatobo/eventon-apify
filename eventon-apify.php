248d1bb643615575f22d45d8654322276520104b
Renato Bonomini <4005901+renatobo@users.noreply.github.com>
2026-03-30 23:14:40 -0700

    Bump version to 1.7.2

--- eventon-apify.php
+++ eventon-apify.php
@@ -3,7 +3,7 @@
  * Plugin Name:       EventON APIfy
  * Plugin URI:        https://github.com/renatobo/eventon-apify
  * Description:       Protected REST API endpoints for EventON events with pagination, CRUD operations, and administrator-only access.
- * Version:           1.7.1
+ * Version:           1.7.2
  * Requires at least: 6.0
  * Requires PHP:      8.0
  * Author:            Renato Bonomini
@@ -24,7 +24,7 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-define('EVENTON_APIFY_VERSION', '1.7.1');
+define('EVENTON_APIFY_VERSION', '1.7.2');
 define('EVENTON_APIFY_NAMESPACE', 'eventonapify/v1');
 define('EVENTON_APIFY_OPTION_ENABLE_API', 'eventon_apify_enable_api');
 define('EVENTON_APIFY_OPTION_API_CAPABILITIES', 'eventon_apify_api_capabilities');
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Tags: eventon, api, rest-api, events
 Requires at least: 6.0
 Tested up to: 6.9.4
 Requires PHP: 8.0
-Stable tag: 1.7.1
+Stable tag: 1.7.2
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
